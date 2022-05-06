5b86e545ae533f08ec9f541f437e3d8d7d58f1ab
tyong0319 <xogud3851@naver.com>
2022-05-06 13:48:28 +0900

    docs : Update README.md
    	;w

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('hello')
+#과제 : 1 부터 10까지 반복하는 과정에서 3의 배수일경우, year를 출력하시오.
+#나머지 모든 경우는 숫자 그대로 출력
+for i in range(1,21):
+    if i % 3 == 0:
+        print('year')
+    else:
+        print(i)
